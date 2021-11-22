3c41f7e18afbb988dcbb2bcba50c507a561585c3
Mesh-254 <Meshack1995@gmail.com>
2021-11-22 15:06:04 +0000

    update

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/bin/usr/python3
 def no_c(my_string):
-    my_string = my_string.translate({ord('c'): None})
-    my_string = my_string.translate({ord('C'): None})
-    return (my_string)
+     new_string = ""
+     for y in my_string:
+         if y != "c" and y != "C":
+              new_string = new_string + 1
+              return new_string
