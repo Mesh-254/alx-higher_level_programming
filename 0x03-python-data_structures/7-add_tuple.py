506cd818f4b5f5650f6d225c6d2f51b6d8e4aae5
Mesh-254 <Meshack1995@gmail.com>
2021-11-22 19:57:55 +0000

    update

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    y = ()
-    for x in (tuple_a, tuple_b):
-        if len(x) == 0:
-              x = (0, 0)
-        elif len(x) == 1:
-             x = (x[0], 0)
-        if y == ():
-            y = x
-            return x[0] + y[0], x[1] + y[1]
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
