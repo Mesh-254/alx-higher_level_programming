2b83e03408b9f8f0de7cbc5ae3ffc79d8d134807
Mesh-254 <Meshack1995@gmail.com>
2021-11-22 20:44:14 +0000

    updates

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,6 @@
 def max_integer(my_list=[]):
     max_value = None
     for i in my_list:
-        if max_value is None or i > Max_value:
-            max_value = i
-            return max_value
+        if max_value is None or i > max_value:
+                max_value = i
+                return max_value
