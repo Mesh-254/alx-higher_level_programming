f0d32f16bb2b1df8a6c38d3f7a32dd92179b9afa
Mesh-254 <Meshack1995@gmail.com>
2021-12-06 15:27:01 +0000

    update

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    lst = 0
+    for x in range(0, x):
+        try:
+            print(my_list[x], end="")
+            lst = lst + 1
+        except:
+            break
+            print("")
+            return(nb)
+
+
+
+
+    
