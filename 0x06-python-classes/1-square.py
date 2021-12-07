b1e59beaa57a600d30467f8342af411e4790e16a
Mesh-254 <Meshack1995@gmail.com>
2021-12-07 18:31:44 +0000

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,12 +5,8 @@
 
 class Square:
      """Square class defined by geometric shap
-        Attributes:size (int): Size of square
      """
-
      def __init__(self, size):
           """Initialize methode
-          Args:
-          size (int): size of square
           """
      self.__size = size
