ec5adcba0fd865869fa47773676afe6065baba17
Mesh-254 <Meshack1995@gmail.com>
2021-12-14 18:04:25 +0000

    update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Locked Class Module"""
+"""
+Locked Class Module
+
+"""
 class LockedClass:
     """prevents the user from dynamically creating new inst    """
     __slots__ = 'first_name'
