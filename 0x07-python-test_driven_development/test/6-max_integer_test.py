610cf7f07934f2e37a679979e389f3ba9c345176
Mesh-254 <Meshack1995@gmail.com>
2021-12-17 23:21:47 +0000

    fixed

---
+++ 0x07-python-test_driven_development/test/6-max_integer_test.py
@@ -0,0 +1,15 @@
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+"""
+A unittest for functions max_integer 
+"""
+
+class TestMaxInteger(unittest.TestCase):
+    """
+    use cases for the functions max_integer tests
+    """
+    self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+    self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+    self.assertEqual(max_integer([1]), 1)
+    self.assertEqual(max_integer([]), None)
+    self.assertRaises(TypeError, max_integer, [1, 2, 3, '4'])
