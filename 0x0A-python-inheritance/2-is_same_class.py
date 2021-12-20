463555a3c0940d64fc46a180e672859a919750b2
Mesh-254 <Meshack1995@gmail.com>
2021-12-20 18:24:45 +0000

    object is exactly an instance of the specified class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,5 @@
 This module contains the function is_same_class
 """
 def is_same_class(obj, a_class):
-    """
-    check instance and class return true if obj is the exact class a_class, else false"""
+    """eturn true if obj is the exact class a_class, else false"""
     return type(obj) == a_class
