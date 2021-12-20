499673b3c7081a9ea98b9a0075890b28dac01f06
Mesh-254 <Meshack1995@gmail.com>
2021-12-20 17:48:33 +0000

    fixed

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """
 contains lookup function
+to check for attributes and methods
 """
 
 def lookup(obj):
-    """returns available attributes and methods
+    """
+    returns available attributes and methods
+  
     """
     return dir(obj)
