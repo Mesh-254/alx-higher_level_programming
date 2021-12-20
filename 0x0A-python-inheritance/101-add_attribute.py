a7f32302ec1293c7fb58d23be2a9cfd55c93603a
Mesh-254 <Meshack1995@gmail.com>
2021-12-20 18:44:37 +0000

    updated

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+adds a new attribute to an object if s possible
+
+"""
+def add_attribute(obj, name, value):
+    """"
+    adds a new attribute to an object if s possible
+    """
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
