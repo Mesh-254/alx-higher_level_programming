97fc1c198130968d1a67a64f30aee7b169ef06df
Mesh-254 <Meshack1995@gmail.com>
2021-12-20 19:07:53 +0000

    fixed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 """
-
     Square Module
-
 """
 
 Rectangle = __import__('9-rectangle').Rectangle
@@ -11,14 +9,13 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Square Class"""
+
     def __init__(self, size):
         """Initialize the square base on Rectangle"""
-        self.__size = size
         self.integer_validator('size', self.__size)
+        self.__size = size
+        super().__init__(size, size)
+
     def area(self):
         """define area of the square"""
         return self.__size ** 2
-    def __str__(self):
-        """__str__ magic methid"""
-        return ("[Rectangle] {}/{}".format(self.__size, self.__size))
-    
