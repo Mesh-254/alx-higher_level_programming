d66ddc6873da7d75a535b2b58bb390e7af2db48f
Mesh-254 <Meshack1995@gmail.com>
2021-12-21 14:18:27 +0000

    fixed

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+function that appends a string at the end of a text file 
+and returns the number of characters added:
+"""
+def append_write(filename="", text=""):
+    """
+    function that appends a string at the end of a text file
+    returns the number of characters added
+    """
+    
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(text)
+    return len(text)
+    
