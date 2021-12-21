cd89d1cde8924c373be2144eed9560337cb3631a
Mesh-254 <Meshack1995@gmail.com>
2021-12-21 19:05:51 +0000

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Module 9-add_item.
-Adds all arguments to a Python list,
-and then save them
 """
 import sys
-
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-open("add_item.json", "a")
 try:
-    l = load_from_json_file("add_item.json")
-except ValueError:
-    l = []
-save_to_json_file(l + sys.argv[1:], "add_item.json")
+    loadFile = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    loadFile = []
+
+argc = len(sys.argv)
+for idx in range(1, argc):
+    loadFile.append(sys.argv[idx])
+save_to_json_file(loadFile, "add_item.json")
