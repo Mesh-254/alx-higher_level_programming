d6d198fd30bbbec588bb9ce6150c08aebb88e111
Mesh-254 <Meshack1995@gmail.com>
2021-12-21 18:46:00 +0000

    fixed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,18 +10,9 @@ from os.path import exists
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-if not exists(filename):
-    open(filename, "w", encoding="utf-8")
-
-saved_list = load_from_json_file(filename)
-to_save_list = []
-
-if saved_list is not None:
-    for e in saved_list:
-        to_save_list.append(e)
-    
-for i in range(1, len(sys.argv)):
-    to_save_list.append(sys.argv[i])
-
-save_to_json_file(to_save_list, filename)
+open("add_item.json", "a")
+try:
+    l = load_from_json_file("add_item.json")
+except ValueError:
+    l = []
+save_to_json_file(l + sys.argv[1:], "add_item.json")
