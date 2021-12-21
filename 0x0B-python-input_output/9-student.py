289ee1b8bba069d62708594e355c9d582dbb6765
Mesh-254 <Meshack1995@gmail.com>
2021-12-21 15:15:45 +0000

    update

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,18 +4,20 @@ A Student class that defines a Studen (module)
 """
 
 
-class Student():
+class Student:
     """
     A Student class that defines a Student
     """
     def __init__(self, first_name, last_name, age):
         """
         INIT
+        initialize the student class
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
+   
         def to_json(self):
             """
             retrieves a dictionary representation
