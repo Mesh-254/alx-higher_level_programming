28a0a82e053cd40a944042f692a84936199d8f49
Mesh-254 <Meshack1995@gmail.com>
2021-12-21 17:43:30 +0000

    fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,10 +3,10 @@
 function that reads a text file (UTF8) and prints it to stdout
 """
 
+
 def read_file(filename=""):
     """
     function that reads a text file (UTF8) and prints it to stdout:
     """
-
     with open(filename, mode='r', encoding="utf-8") as f:
         print(f.read(), end="")
