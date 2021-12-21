6badb7c4c2ad837b9fa5df2b16b880894d5a4ea5
Mesh-254 <Meshack1995@gmail.com>
2021-12-21 14:25:49 +0000

    fixed:

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" a function that serializes object"""
+import json
+
+def to_json_string(my_obj):
+    """ a function that serializes in python"""
+    return json.dumps(my_obj)
