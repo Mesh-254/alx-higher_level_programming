4870613bf058c74977a79da7f20282a9b2c80532
Mesh-254 <Meshack1995@gmail.com>
2021-12-21 18:01:45 +0000

    function to write a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,10 +3,7 @@
 
 
 def write_file(filename="", text=""):
-    """
-    function that writes a string to a text file (UTF8) 
-    returns the number of characters written:
-    """
+    """function that writes a string to a text file (UTF8)"""
     with open (filename, "w", encoding="utf-8") as f:
         f.write(text)
-    return len(text)
+        return len(text)
