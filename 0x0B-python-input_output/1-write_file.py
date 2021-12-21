d9ddda52e4699a6386c206fc570a5d3028a5e70f
Mesh-254 <Meshack1995@gmail.com>
2021-12-21 18:32:40 +0000

    :fixed

--- 0x0B-python-input_output/1-write_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""function that writes a string to a text file (UTF8)"""
-
-
-def write_file(filename="", text=""):
-    """function that writes a string to a text file (UTF8)"""
-    with open (filename, "w", encoding="utf-8") as f:
-        f.write(text)
-        return len(text)
--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,6 +2,7 @@
 """ a function that writes json to file"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """Writes an Object to a text file, using a JSON representation."""
     with open(filename, 'w', encoding='utf-8') as f:
