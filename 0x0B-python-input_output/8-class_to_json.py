8dcc3802086a151ae5a62363bbb2c30c8f5c9aff
Mesh-254 <Meshack1995@gmail.com>
2021-12-21 18:53:37 +0000

    fic\xed

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,12 +1,7 @@
-B#!/usr/bin/python3
-"""
-function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean)for JSON serialization of an object:
-"""
+#!/usr/bin/python3
+"""function to convert args to dict"""
 
 
 def class_to_json(obj):
-    """
-    returns the dictionary description with simple data structure
-    ( integer and boolean) for JSON serialization of an object:
-    """
+    """ a function to convert args to dict"""
     return(obj.__dict__)
