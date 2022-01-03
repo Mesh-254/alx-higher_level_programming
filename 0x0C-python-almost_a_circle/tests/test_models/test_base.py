ee25cb3b5f9ffbbf83e20c48b3dedb4437f8d6f5
Mesh-254 <Meshack1995@gmail.com>
2022-01-03 11:01:48 +0000

    update

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Unittest for class Base
+"""
+from unittest import TestCase
+from models.base import Base
+
+
+class TestBase(TestCase):
+    """unittest for class TestBase"""
+    
+     def test_has_id(self):
+         """ unittest for attribute id"""
+         base = Base()
+         self.assertEqual(base.id, 1)
+
+     def test_to_json_string(self):
+         actual = []
+         self.fail()
+
+     def test_save_to_file(self):
+         self.fail()
+        
+     def test_from_json_string(self):
+         self.fail()
+
+     def 
