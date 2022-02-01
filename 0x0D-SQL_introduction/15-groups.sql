7a599d671057524d505a0045f0076cead6699a2d
Mesh-254 <Meshack1995@gmail.com>
2022-02-01 07:59:02 -0800

    lists the number of records with the same score in the table second_table

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0
-SELECT   score, count(`score`) as number FROM second_table GROUP BY scor ORDER BY number DESC;
+SELECT  `score`, count(`score`) AS number FROM `second_table` GROUP BY `score` ORDER BY `number` DESC;
 
